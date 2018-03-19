d6cc79754d3b39814cb1e8277bcb81a0673f448b
Anıl Ulak <anil_ulak@hotmail.com>
2018-03-20 00:43:06 +0300

    ReadActivity is added and after that an error occured: 1)add the xmlns:tools line in the manifest tag 2)add tools:replace in the application tag
    and i solved with these steps.

---
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/ReadActivity.java
@@ -0,0 +1,15 @@
+package nfciue.nfcsmartanimalpassport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadActivity extends AppCompatActivity {
+
+    private String m_strKey = "a8ecf9cd118701724d7a24c76c9495f0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+    }
+}

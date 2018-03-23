813e5672a2164dc903eab7e20f7b6d97e7bfa1da
Anıl Ulak <anil_ulak@hotmail.com>
2018-03-23 12:23:03 +0300

    Sample read-write and password protection module works.

--- app/src/main/java/nfciue/nfcsmartanimalpassport/WriteToTagActivity.java
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/WriteToTagActivity.java
@@ -13,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nxp.nfclib.CardType;
@@ -29,9 +28,11 @@ import com.nxp.nfclib.ntag.NTagFactory;
 import java.io.ByteArrayOutputStream;
 import java.util.Locale;
 
-public class WriteActivity extends AppCompatActivity {
-
+public class WriteToTagActivity extends AppCompatActivity {
     final Context context = this;
+
+    byte[] myPass;
+    String textToNFC;
 
     // The license key, got from TapLinx.
     private String m_strKey = "a8ecf9cd118701724d7a24c76c9495f0";
@@ -48,78 +49,13 @@ public class WriteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_write);
-
-        final EditText writeEditText = (EditText) findViewById(R.id.writeEditText);
+        setContentView(R.layout.activity_write_to_tag);
 
         initializeLibrary();    // Initialize library.
 
-        Button writeToTagButton = (Button) findViewById(R.id.writeToTagButton);
-        writeToTagButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                // Get string from edittext.
-                final String textToNFC = writeEditText.getText().toString();
-
-                // get prompts.xml view
-                LayoutInflater li = LayoutInflater.from(context);
-                View promptsView = li.inflate(R.layout.prompts, null);
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        context);
-
-                // set prompts.xml to alertdialog builder
-                alertDialogBuilder.setView(promptsView);
-
-                final EditText userInput1 = (EditText) promptsView
-                        .findViewById(R.id.editTextDialogUserInput1);
-                final EditText userInput2 = (EditText) promptsView
-                        .findViewById(R.id.editTextDialogUserInput2);
-                final EditText userInput3 = (EditText) promptsView
-                        .findViewById(R.id.editTextDialogUserInput3);
-                final EditText userInput4 = (EditText) promptsView
-                        .findViewById(R.id.editTextDialogUserInput4);
-
-                // set dialog message
-                alertDialogBuilder
-                        .setCancelable(false)
-                        .setPositiveButton("OK",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        // get user input and set it to result
-                                        // edit text
-                                        //result.setText(userInput.getText());
-                                        Intent WriteToTagIntent = new Intent(WriteActivity.this, WriteToTagActivity.class);
-                                        WriteToTagIntent.putExtra("textToNFC", textToNFC);
-
-                                        int digit1 = Integer.parseInt(userInput1.getText().toString());
-                                        int digit2 = Integer.parseInt(userInput2.getText().toString());
-                                        int digit3 = Integer.parseInt(userInput3.getText().toString());
-                                        int digit4 = Integer.parseInt(userInput4.getText().toString());
-
-                                        byte[] myPass = new byte[] {(byte) digit1, (byte) digit2, (byte) digit3, (byte) digit4};
-                                        WriteToTagIntent.putExtra("myPass", myPass);
-
-                                        WriteToTagIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        startActivity(WriteToTagIntent);
-
-                                    }
-                                })
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-
-                // create alert dialog
-                AlertDialog alertDialog = alertDialogBuilder.create();
-
-                // show it
-                alertDialog.show();
-            }
-        });
+        myPass = getIntent().getByteArrayExtra("myPass");
+        textToNFC = getIntent().getStringExtra("textToNFC");
     }
 
     // Called if app becomes active.
@@ -155,7 +91,7 @@ public class WriteActivity extends AppCompatActivity {
         switch (type) {
             case NTag216:
                 try {
-                    ntagCardLogic(NTagFactory.getInstance().getNTAG216(m_libInstance.getCustomModules()));
+                    ntagCardLogic(NTagFactory.getInstance().getNTAG216(m_libInstance.getCustomModules()), textToNFC, myPass);
                 } catch (Throwable t) {
                     Log.e("ULAK", "Error in switch(NTag216) " + t.getMessage());
                 }
@@ -166,7 +102,7 @@ public class WriteActivity extends AppCompatActivity {
         }
     }
 
-    private void ntagCardLogic(final INTag tag) {
+    private void ntagCardLogic(final INTag tag, String dataTextToNFC, byte[] pass) {
         Log.e("ULAK", "ntagCardLogic'in içi");
 
 
@@ -175,12 +111,13 @@ public class WriteActivity extends AppCompatActivity {
             tag.getReader().connect();
             NTag213215216 ntag216 = (NTag213215216) tag;
 
-            byte[] myPassword = new byte[]{(byte) 1, (byte) 2, (byte) 3, (byte) 4};
+            //byte[] myPassword = new byte[]{(byte) 1, (byte) 2, (byte) 3, (byte) 4};
+            byte[] myPassword = pass;
             byte[] myAck = new byte[] {(byte) 0x00, (byte) 0x00};
             byte[] defPass = new byte[]{(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff};
 
 
-            String ndefMessage = "mustafakemal";
+            String ndefMessage = dataTextToNFC;
 
             byte[] lang = Locale.getDefault().getLanguage().getBytes("UTF-8");
             byte[] text = ndefMessage.getBytes("UTF-8"); // Content in UTF-8
@@ -201,13 +138,14 @@ public class WriteActivity extends AppCompatActivity {
             NdefMessageWrapper nmw = new NdefMessageWrapper(nrw);
             INdefMessage nm = nmw;
 
+            ntag216.authenticatePwd(myPassword, myAck);
             ntag216.writeNDEF(nm);
 
 
 
-            Toast.makeText(this, "OKEY!!!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "OK from WTTA!!!", Toast.LENGTH_LONG).show();
         } catch (Exception e) {
-            Log.e("ULAK", "Error in ntagCardLogic: " + e.getMessage());
+            Log.e("ULAK", "Error in ntagCardLogic: Password is wrong... " + e.getMessage());
         } finally {
             Log.e("ULAK", "finally'nin içi.");
             tag.getReader().close();
@@ -215,4 +153,5 @@ public class WriteActivity extends AppCompatActivity {
 
         }
     }
+
 }

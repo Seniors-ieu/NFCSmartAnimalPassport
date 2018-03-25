69293f0a6ba241743545d3616678de11472d04ad
Anıl Ulak <anil_ulak@hotmail.com>
2018-03-25 22:55:19 +0300

    Activities for read operations are added.

---
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/PartialInfoShownActivity.java
@@ -0,0 +1,13 @@
+package nfciue.nfcsmartanimalpassport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PartialInfoShownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partial_info_shown);
+    }
+}

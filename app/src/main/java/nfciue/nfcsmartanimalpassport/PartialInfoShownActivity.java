1062b16c0dad68767116598a83d503d4f2a8ea14
Anıl Ulak <anil_ulak@hotmail.com>
2018-03-26 21:43:48 +0300

    Began to add textviews to activity. not finished, asli will continue.

--- app/src/main/java/nfciue/nfcsmartanimalpassport/PartialInfoShownActivity.java
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/PartialInfoShownActivity.java
@@ -2,12 +2,37 @@ package nfciue.nfcsmartanimalpassport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class PartialInfoShownActivity extends AppCompatActivity {
+    TextView textViewAnimalID;
+    TextView textViewBirthDate;
+    TextView textViewGender;
+    TextView textViewBreed;
+    TextView textViewMotherID;
+    TextView textViewBirthFarmNo;
+    TextView textViewCurrentFarmNo;
+    TextView textViewFarmChangeDate;
+    TextView textViewOwnerTc;
+    TextView textViewOwnerNameLastName;
+    TextView textViewOwnerAdress;
+    TextView textViewFarmGeoCoordinates;
+    TextView textViewFarmCountryCode;
+    TextView textViewFarmCityCode;
+    TextView textViewFarmAddress;
+    TextView textViewFarmPhone;
+    TextView textViewFarmFax;
+    TextView textViewFarmEMail;
+    TextView textViewExportCountryCode;
+    TextView textViewExportDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partial_info_shown);
+        textViewAnimalID = (TextView) findViewById(R.id.textViewAnimalID);
+
     }
 }

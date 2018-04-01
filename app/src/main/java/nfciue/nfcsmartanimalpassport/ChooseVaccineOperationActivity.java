fe276f674e393350ddd8bf8077ea4a0819a608e9
Aslıhan Öztürk <aslihan-ozturk@hotmail.com>
2018-04-01 12:24:28 +0300

    Layout file that will be displayed after Write button of login activity is clicked

---
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/ChooseVaccineOperationActivity.java
@@ -0,0 +1,13 @@
+package nfciue.nfcsmartanimalpassport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseVaccineOperationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_vaccine_operation);
+    }
+}

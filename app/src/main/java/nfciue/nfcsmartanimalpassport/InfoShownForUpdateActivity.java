8f56b757f38f7ce8036e4166dd9a47e024ab1c41
Aslıhan Öztürk <aslihan-ozturk@hotmail.com>
2018-04-02 01:49:15 +0300

    only other vaccine info is sent to db This commit will be edited for alum-brusella-pasteur vaccine

--- app/src/main/java/nfciue/nfcsmartanimalpassport/InfoShownForUpdateActivity.java
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/InfoShownForUpdateActivity.java
@@ -30,7 +30,7 @@ public class InfoShownForUpdateActivity extends AppCompatActivity {
 
 
     Button buttonUpdateDb;
-
+    String vaccineNameFromPrevActivity;
     String opNameFromPrevActivity;
     String animalIDFromPrevActivity;
     String animalIdForUpdate="TR 35 123"; // todo: bu kısım tagden doğru animal ID alındığında kaldırılıcak. ilgili yerlerde animalIdFroPRevActivity kullanılacak. Aslı Fİrebasei düzenleyecek..
@@ -44,16 +44,19 @@ public class InfoShownForUpdateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info_shown_for_update);
         final Context context = this;
         opNameFromPrevActivity = getIntent().getStringExtra("OperationName");
+        vaccineNameFromPrevActivity = getIntent().getStringExtra("VaccineName");
         animalIDFromPrevActivity = getIntent().getStringExtra("AnimalID");
 
 
+        if(vaccineNameFromPrevActivity.equals("NA"))
+        {
 
         final Animal animal = new Animal();    //burada veritabanından tam okuma yapılabilir, internet olmadıkça bu aktivitynn çalışması zaten mümkün değil.
         animal.setAlumVaccine(true);
         animal.setBirthdate("16.05.2010");
         animal.setBirthFarmNo("55567533");
         animal.setBreed("Holstein");
-        animal.setBrusellosisVaccine(false);
+        animal.setBrusellosisVaccine(true);
         animal.setCurrentFarmNo("1234567");
         animal.setDeathDate("NA");
         animal.setDeathPlace("NA");
@@ -63,13 +66,13 @@ public class InfoShownForUpdateActivity extends AppCompatActivity {
         animal.setExportDate("NA");
         animal.setFarmChangeDate("NA");
         animal.setiD("TR 35 123");
-        animal.setFemale(false);
+        animal.setFemale(true);
         animal.setMotherId("TR 35 122");
 
 
         readVaccineForUpdate(new VaccineCallback() { //All vaccines will be retrieved from db, tg only have the last one...
             @Override
-            public void onCallback( ArrayList <otherVaccine> otherVaccines) {     //otherVaccines veritabanından gelen aşılar
+            public void onCallback(ArrayList<otherVaccine> otherVaccines) {     //otherVaccines veritabanından gelen aşılar
                 animal.setOtherVaccine(otherVaccines);
             }
         });
@@ -77,7 +80,7 @@ public class InfoShownForUpdateActivity extends AppCompatActivity {
 
         readOperationForUpdate(new OperationCallback() { //All operations will be retrieved from db, tg only have the last one...
             @Override
-            public void onCallback(ArrayList <Operations> operations) {     //otherVaccines veritabanından gelen aşılar
+            public void onCallback(ArrayList<Operations> operations) {     //otherVaccines veritabanından gelen aşılar
                 animal.setOperations(operations);
             }
         });
@@ -95,12 +98,11 @@ public class InfoShownForUpdateActivity extends AppCompatActivity {
             public void onClick(View arg0) {
 
 
+                Operations operation = new Operations(opNameFromPrevActivity, "1111111", new Date());
+                animal.getOperations().add(operation);
 
-                        Operations operation = new Operations(opNameFromPrevActivity,"1111111",new Date());
-                        animal.getOperations().add(operation);
-
-                        FirebaseFirestore db = FirebaseFirestore.getInstance();
-                        db.collection("Animals").document(animal.getiD()).set(animal);
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("Animals").document(animal.getiD()).set(animal);
                 AlertDialog alertDialog = new AlertDialog.Builder(InfoShownForUpdateActivity.this).create();
                 alertDialog.setTitle("Success");
                 alertDialog.setMessage("Database is updated successfully");
@@ -114,19 +116,84 @@ public class InfoShownForUpdateActivity extends AppCompatActivity {
                 alertDialog.show();
 
 
-                    }
-                });
+            }
+        });
 
 
+    }
+    else if(opNameFromPrevActivity.equals("NA"))
+    {
+        final Animal animal = new Animal();    //burada veritabanından tam okuma yapılabilir, internet olmadıkça bu aktivitynn çalışması zaten mümkün değil.
+
+        animal.setBirthdate("16.05.2010");
+        animal.setBirthFarmNo("55567533");
+        animal.setBreed("Holstein");
+
+        animal.setCurrentFarmNo("1234567");
+        animal.setDeathDate("NA");
+        animal.setDeathPlace("NA");
+        animal.seteSignDirector("14567SFGHJK4567");
+        animal.seteSignOwner("1235WRTY357");
+        animal.setExportCountryCode(0);
+        animal.setExportDate("NA");
+        animal.setFarmChangeDate("NA");
+        animal.setiD("TR 35 123");
+        animal.setFemale(false);
+        animal.setMotherId("TR 35 122");
 
 
+        readVaccineForUpdate(new VaccineCallback() { //All vaccines will be retrieved from db, tg only have the last one...
+            @Override
+            public void onCallback(ArrayList<otherVaccine> otherVaccines) {     //otherVaccines veritabanından gelen aşılar
+                animal.setOtherVaccine(otherVaccines);
+            }
+        });
 
 
+        readOperationForUpdate(new OperationCallback() { //All operations will be retrieved from db, tg only have the last one...
+            @Override
+            public void onCallback(ArrayList<Operations> operations) {     //otherVaccines veritabanından gelen aşılar
+                animal.setOperations(operations);
+            }
+        });
+
+        animal.setOwnerTc("11111111111");
+        animal.setAlumVaccine(true);
+        animal.setBrusellosisVaccine(true);
+        animal.setPasturellaVaccine(true);// with the information that is retrieved from Tag, initialize animal with parameters. All fields should match with db , othervise use call back to retrieve whole object from db.. (aslı)
+        //Owner info won't be updated by user for now, if this changes, initialize an owner also.
 
 
+        buttonUpdateDb = findViewById(R.id.buttonUpdateDb);
 
 
+        buttonUpdateDb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
 
+
+                otherVaccine otherVaccine = new otherVaccine(new Date(),vaccineNameFromPrevActivity);
+                animal.getOtherVaccine().add(otherVaccine);
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("Animals").document(animal.getiD()).set(animal);
+                AlertDialog alertDialog = new AlertDialog.Builder(InfoShownForUpdateActivity.this).create();
+                alertDialog.setTitle("Success");
+                alertDialog.setMessage("Database is updated successfully");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+
+
+            }
+        });
+
+    }
 
     }
     public interface VaccineCallback {

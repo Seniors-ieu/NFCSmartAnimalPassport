75602ffbfc50e326467110ba0e34da7a6cdf77f3
Aslıhan Öztürk <aslihan-ozturk@hotmail.com>
2018-04-21 23:36:54 +0300

    this will be new info shown activity after all problems are solved.

---
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/ExpListAdapter.java
@@ -0,0 +1,10 @@
+package nfciue.nfcsmartanimalpassport;
+
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ * Created by ASLI on 20.4.2018.
+ */
+
+public class ExpListAdapter extends BaseExpandableListAdapter {
+}
---
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/InfoShownInExpandedListActivity.java
@@ -0,0 +1,13 @@
+package nfciue.nfcsmartanimalpassport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoShownInExpandedListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_shown_in_expanded_list);
+    }
+}

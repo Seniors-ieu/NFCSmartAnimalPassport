04bf28a093285d345017739f89c57e28b75d0e61
Aslıhan Öztürk <aslihan-ozturk@hotmail.com>
2018-04-23 01:54:47 +0300

    navigation bar is added. Vaccine and operation info is shown in expandable list.

--- app/src/main/java/nfciue/nfcsmartanimalpassport/InfoShownInExpandedListActivity.java
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/InfoShownInExpandedListActivity.java
@@ -1,10 +1,18 @@
 package nfciue.nfcsmartanimalpassport;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,6 +27,8 @@ public class InfoShownInExpandedListActivity extends AppCompatActivity {
     ExpListAdapter adapter;
     private String dataFromNFCTag;
     private String[] parsedDataFromNFCTag;
+    BottomNavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,7 @@ public class InfoShownInExpandedListActivity extends AppCompatActivity {
         items = new HashMap<String, ArrayList<String>>();
         data = new ArrayList<ArrayList<String>>();
 
-        dataFromNFCTag = getIntent().getStringExtra("dataFromReadActivityIntent");
+        dataFromNFCTag = getIntent().getStringExtra("dataFromPartialInfoShownActivityIntent");
         try {
             parsedDataFromNFCTag = Decoderiue.parseNFCData(dataFromNFCTag);
         } catch (Exception e) {
@@ -67,8 +77,14 @@ public class InfoShownInExpandedListActivity extends AppCompatActivity {
         data.add(farm);
 
         ArrayList<String> health= new ArrayList<String>();
-        health.add("Vaccines");
-        health.add("Operations");
+
+        health.add(parsedDataFromNFCTag[12] + "\nTarih:" + parsedDataFromNFCTag[43]);
+        health.add( parsedDataFromNFCTag[14]); //+ "\nTarih:" + parsedDataFromNFCTag[45]); todo: stringe eklenmeli (Anıl)
+        health.add(parsedDataFromNFCTag[13] + "\nTarih:" + parsedDataFromNFCTag[44]);
+        health.add(Decoderiue.vaccineChooser(parsedDataFromNFCTag[15])+ "\nTarih:" + parsedDataFromNFCTag[42]);
+
+        health.add(Decoderiue.operationChooser(parsedDataFromNFCTag[33]) + "\nTarih:" + parsedDataFromNFCTag[41]);
+        health.add("");
         data.add(health);
 
         manageGroupsAndItems();
@@ -76,9 +92,56 @@ public class InfoShownInExpandedListActivity extends AppCompatActivity {
 
         adapter = new ExpListAdapter(this, groups, items,data);
         expListView.setAdapter(adapter);
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+
+                switch(i){
+                    case 3:{
+                        switch(i1){
+                            case 5: Toast.makeText(InfoShownInExpandedListActivity.this,"heyheyehey",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                return true;
+            }
+        });
         Log.e("adapter","set");
+
+
+        navigationView = findViewById(R.id.navigationInfoShown);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               switch(item.getItemId()){
+                   case R.id.action_backToMain:
+                       if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+                           Intent ExpListIntent = new Intent(InfoShownInExpandedListActivity.this, SignedInMainActivity.class);
+                           ExpListIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                           startActivity(ExpListIntent);
+                       }
+                       else{
+                           Intent InfoShownIntent = new Intent(InfoShownInExpandedListActivity.this, MainActivity.class);
+                           startActivity(InfoShownIntent);
+                       } break;
+                   case R.id.action_profile:
+                       break;
+                   case R.id.action_edit:
+                       if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+                           Intent ExpListIntent = new Intent(InfoShownInExpandedListActivity.this, ChooseVaccineOperationActivity.class);
+                           ExpListIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                           startActivity(ExpListIntent);
+                       }
+                       else{
+                           Toast.makeText(InfoShownInExpandedListActivity.this,"You should be logged in for this!",Toast.LENGTH_SHORT).show();
+                       } break;
+               }
+               return true;
+
+            }});
+
     }
-    public void manageGroupsAndItems(){
+        public void manageGroupsAndItems(){
 
 
 
@@ -116,8 +179,12 @@ public class InfoShownInExpandedListActivity extends AppCompatActivity {
         farmInfo.add("E-mail");
 
         ArrayList<String> healthInfo= new ArrayList<String>();
-        healthInfo.add("Vaccines");
-        healthInfo.add("Operations");
+        healthInfo.add("Alum Vaccine");
+        healthInfo.add("Pasteurella Vaccine");
+        healthInfo.add("Brucellosis Vaccine");
+        healthInfo.add("Other Vaccine");
+        healthInfo.add("Operation");
+        healthInfo.add("Click for more detail...");
 
 
 
@@ -131,8 +198,10 @@ public class InfoShownInExpandedListActivity extends AppCompatActivity {
 
 
     }
+    @Override
+    public void onBackPressed() {
 
-
+    }
 
 }
 

c9080ec86c31f13c606997928554f4e8e17e1762
Aslıhan Öztürk <aslihan-ozturk@hotmail.com>
2018-04-27 18:02:46 +0300

    signed iin main is changed to get user name from firebase to display welcome message and sign out function

--- app/src/main/java/nfciue/nfcsmartanimalpassport/SignedInMainActivity.java
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/SignedInMainActivity.java
@@ -3,51 +3,102 @@ package nfciue.nfcsmartanimalpassport;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignedInMainActivity extends AppCompatActivity {
 
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    private Button buttonWrite;
-    private Button buttonRead;
-    private Button buttonSignOut;
-
+    BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_signed_in_main);
+        setContentView(R.layout.activity_signed_in_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-        if(FirebaseAuth.getInstance().getCurrentUser()==null)
-        {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             Intent NotSignedIntent = new Intent(SignedInMainActivity.this, MainActivity.class);
             NotSignedIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(NotSignedIntent);
+        } else {
+            Log.e("oncreatesignedinmain", user.getUid());
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference docRef = db.collection("Users").document(user.getUid());
+            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    Users usr = documentSnapshot.toObject(Users.class);
+                    TextView textView = findViewById(R.id.textViewWelcome);
+                    textView.setText("Welcome back  "+ usr.getNameLastname());
+                }
+            });
+            //getSupportActionBar().setTitle("Welcome, " + user.getEmail());
         }
-        else
-        {
-
-            getSupportActionBar().setTitle("Welcome, "+user.getEmail());
-        }
 
 
+        navigationView=findViewById(R.id.navigationSignedInMain);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.action_signout:
+
+                        FirebaseAuth.getInstance().signOut();
+                        AlertDialog alertDialog = new AlertDialog.Builder(SignedInMainActivity.this).create();
+                        alertDialog.setTitle("Info");
+                        alertDialog.setMessage("You signed out");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+
+                        Intent MainIndent = new Intent(SignedInMainActivity.this, MainActivity.class);
+                        MainIndent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(MainIndent);
+
+                        break;
+                    case R.id.action_read:
+                        Intent ReadIntent = new Intent(SignedInMainActivity.this, ReadActivity.class);
+                        ReadIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(ReadIntent);
+                        break;
+                    case R.id.action_edit:
+                        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+                            Intent EditIntent = new Intent(SignedInMainActivity.this, ChooseVaccineOperationActivity.class);
+                            EditIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(EditIntent);
+                        }
+                        else{
+                            Toast.makeText(SignedInMainActivity.this,"You should be logged in for this!",Toast.LENGTH_SHORT).show();
+                        } break;
+                }
+                return true;
+
+            }});
 
 
 
-
-        buttonWrite=findViewById(R.id.buttonWrite);
+      /*  buttonWrite=findViewById(R.id.buttonWrite);
         buttonRead=findViewById(R.id.buttonRead);
         buttonSignOut=findViewById(R.id.buttonSignOut);
 
@@ -99,7 +150,7 @@ public class SignedInMainActivity extends AppCompatActivity {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
                 if (user != null) {
-                    // User is signed in
+                    // Users is signed in
                     Intent WriteIntent = new Intent(SignedInMainActivity.this,  ChooseVaccineOperationActivity.class);
                     WriteIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(WriteIntent);
@@ -121,6 +172,7 @@ public class SignedInMainActivity extends AppCompatActivity {
             }
         });
 
+    */
     }
     @Override
     public void onBackPressed() {

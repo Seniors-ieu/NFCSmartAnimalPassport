8be4624eeff5d1a47f5a080675ed7cf9b6e2d88f
Aslıhan Öztürk <aslihan-ozturk@hotmail.com>
2018-04-27 18:04:15 +0300

    users class is required to hold nformation about user: such as name , last name.

--- app/src/main/java/nfciue/nfcsmartanimalpassport/Users.java
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/Users.java
@@ -4,6 +4,29 @@ package nfciue.nfcsmartanimalpassport;
  * Created by ASLI on 27.4.2018.
  */
 
-class User {
-    private nameLastna
+class Users {
+    private String nameLastname;
+    private String UID;
+
+    public Users(String nameLastname, String UID) {
+        this.nameLastname = nameLastname;
+        this.UID = UID;
+    }
+
+    public String getNameLastname() {
+        return nameLastname;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setNameLastname(String nameLastname) {
+        this.nameLastname = nameLastname;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+    public Users(){}
 }

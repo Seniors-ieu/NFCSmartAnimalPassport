5242aef223cad15545c2edbed3b377ac67318f93
Aslıhan Öztürk <aslihan-ozturk@hotmail.com>
2018-05-20 16:59:13 +0300

    operasyon için diğer seçeneği eklendi

--- app/src/main/java/nfciue/nfcsmartanimalpassport/ChooseVaccineOperationActivity.java
+++ app/src/main/java/nfciue/nfcsmartanimalpassport/ChooseVaccineOperationActivity.java
@@ -66,6 +66,7 @@ public class ChooseVaccineOperationActivity extends AppCompatActivity {
         vaccines.add("Buzağı Septisemi Serumu Aşısı");
         vaccines.add("Brucellosis Aşısı");
 
+
         ArrayList<String> operations = new ArrayList<String>();
         operations.add("Kan Analizi");
         operations.add("Pansuman");
@@ -73,6 +74,7 @@ public class ChooseVaccineOperationActivity extends AppCompatActivity {
         operations.add("Fiziksel Muayene");
         operations.add("Medikal Operasyon");
         operations.add("Doğum");
+        operations.add("Diğer");
 
         items.put(groups.get(0), vaccines);
         items.put(groups.get(1), operations);
@@ -86,48 +88,47 @@ public class ChooseVaccineOperationActivity extends AppCompatActivity {
                 switch(i){
                     case 0:{
                         switch(i1){
-                            case 0: Toast.makeText(ChooseVaccineOperationActivity.this,"heyheyehey",Toast.LENGTH_SHORT).show();
-                                    vaccineCode=1; break;
+                            case 0: vaccineCode=1; break;
                             case 1: vaccineCode=2; break;
                             case 2: vaccineCode=3; break;
                             case 3: vaccineCode=4; break;
                             case 4: vaccineCode=5; break;
                             case 5: vaccineCode=6; break;
+
                         }
                     }  break;
                     case 1:{
                         switch(i1){
-                            case 0: Toast.makeText(ChooseVaccineOperationActivity.this,"heyheyeheyoperation",Toast.LENGTH_SHORT).show();
-                                    operationCode=1; break;
+                            case 0: operationCode=1; break;
                             case 1: operationCode=2; break;
                             case 2: operationCode=3; break;
                             case 3: operationCode=4; break;
                             case 4: operationCode=5; break;
                             case 5: operationCode=6; break;
+                            case 6: operationCode=7; break; // diğer seçeneği
                         }
                     }
                 }
 
                 if(vaccineCode==-1){  //operation eklenmiş
-                    final Dialog dialogComment = new Dialog(context);
-                    dialogComment.setContentView(R.layout.add_comment);
-                    dialogComment.setTitle("Lütfen Şifrenizi Doğrulayın ve Varsa Notlarınızı Ekleyin");
+                    if(operationCode==7){ // not bırakmadan geçmemeli, diğer seçeneği
+                        final Dialog dialogComment1 = new Dialog(context);
+                        dialogComment1.setContentView(R.layout.add_comment);
+                        dialogComment1.setTitle("Lütfen Şifrenizi Doğrulayın ve Operasyonu açıklayın.");
 
-                    final EditText editTextComment = dialogComment.findViewById(R.id.editTextComment);
-                    final EditText editTextPsw =dialogComment.findViewById(R.id.confirmPassword);
-                    Button buttonOkComment = dialogComment.findViewById(R.id.buttonOk);
+                        final EditText editTextComment1= dialogComment1.findViewById(R.id.editTextComment);
+                        final EditText editTextPsw1 =dialogComment1.findViewById(R.id.confirmPassword);
+                        Button buttonOkComment1 = dialogComment1.findViewById(R.id.buttonOk);
 
-                    buttonOkComment.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
+                        buttonOkComment1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
 
-                            commentForOperation = editTextComment.getText().toString();
-                            if(commentForOperation.equals("")){
-                                commentForOperation="Not bilgisi bulunmamaktadır.";
-                            }
-
-
-                                    String password = editTextPsw.getText().toString();
+                                commentForOperation = editTextComment1.getText().toString();
+                                if(commentForOperation.trim().equals(null)|| commentForOperation.equals("") ){
+                                   Toast.makeText(context,"Diğer operasyonlar için not kısmı boş bırakılamaz. Detayları açıklayınız.",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    String password = editTextPsw1.getText().toString();
 
                                     Intent ChooseVaccineOpIntent = new Intent(ChooseVaccineOperationActivity.this, WriteToTagActivity.class);
                                     ChooseVaccineOpIntent.putExtra("password", password);
@@ -136,12 +137,51 @@ public class ChooseVaccineOperationActivity extends AppCompatActivity {
                                     ChooseVaccineOpIntent.putExtra("opComment", commentForOperation);
                                     ChooseVaccineOpIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                     startActivity(ChooseVaccineOpIntent);
-
                                 }
-                            });
 
-                    dialogComment.show();
 
+
+
+                            }
+                        });
+
+                        dialogComment1.show();
+                    }else if(operationCode<7){
+                        final Dialog dialogComment = new Dialog(context); //op type belirtilmis, comment zorunlu değil
+                        dialogComment.setContentView(R.layout.add_comment);
+                        dialogComment.setTitle("Lütfen Şifrenizi Doğrulayın ve Varsa Notlarınızı Ekleyin");
+
+                        final EditText editTextComment = dialogComment.findViewById(R.id.editTextComment);
+                        final EditText editTextPsw =dialogComment.findViewById(R.id.confirmPassword);
+                        Button buttonOkComment = dialogComment.findViewById(R.id.buttonOk);
+
+                        buttonOkComment.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                commentForOperation = editTextComment.getText().toString();
+                                if(commentForOperation.equals("")){
+                                    commentForOperation="Not bilgisi bulunmamaktadır.";
+                                }
+
+
+                                String password = editTextPsw.getText().toString();
+
+                                Intent ChooseVaccineOpIntent = new Intent(ChooseVaccineOperationActivity.this, WriteToTagActivity.class);
+                                ChooseVaccineOpIntent.putExtra("password", password);
+                                ChooseVaccineOpIntent.putExtra("operationCode", operationCode);
+                                ChooseVaccineOpIntent.putExtra("vaccineCode", vaccineCode);
+                                ChooseVaccineOpIntent.putExtra("opComment", commentForOperation);
+                                ChooseVaccineOpIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(ChooseVaccineOpIntent);
+
+                            }
+                        });
+
+                        dialogComment.show();
+
+
+                    }
 
 
                 }
